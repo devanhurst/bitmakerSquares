26e70365d90286ce2c111fd754ddb889372a1528
devanhurst <devan.hurst@gmail.com>
2015-11-26 20:32:20 -0500

    5 square win and repetition prevention.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ $('document').ready(function(){
     directory = window.location.pathname;
     directory = directory.substring(0, directory.lastIndexOf('/')) + "/";
     var students = ["alon", "andrew", "avi", "carlos", "colin", "dan", "devan", "hershel", "james", "jeff", "jong", "jonk", "jordan", "lauren", "margaret", "matt", "mike", "patrick", "rex", "rob", "ryan", "scott", "steph", "tyler", "zain"];
-    players = ["devan", "alon", "andrew", "ryan", "lauren", "tyler", "matt", "margaret", "mike"];
+
+
+    players = ["devan", "alon", "avi", "ryan", "lauren", "tyler", "matt", "margaret", "mike"];
 
     document.getElementById("1").src = "images/default/" + players[0] + ".png";
     document.getElementById("2").src = "images/default/" + players[1] + ".png";
@@ -26,6 +28,9 @@ $('document').ready(function(){
     win.setAttribute('src', 'win.mp3');
 
     function isWinner(array) {
+        if ( array.length == 5 ) {
+            return true;
+        }
         var winCombos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
         for ( i = 0; i < winCombos.length; i++ ) {
             item = winCombos[i];
@@ -54,65 +59,68 @@ $('document').ready(function(){
     }
 
     $( ".box").click(function() {
-        if ( deciding == false ){
-            deciding = true;
-            index = this.id - 1;
-            idName = "#" + this.id
-            var flashing = setInterval(function(){
-                d = new Date();
-                if ( $(idName)[0].src === "file://" + directory + "images/default/" + players[index] + ".png" ) {
-                    console.log(true);
+        console.log(this);
+        index = this.id - 1;
+        if ( texts[index].innerHTML == "" ) {
+            if ( deciding == false ){
+                deciding = true;
+                idName = "#" + this.id
+                var flashing = setInterval(function(){
+                    d = new Date();
+                    if ( $(idName)[0].src === "file://" + directory + "images/default/" + players[index] + ".png" ) {
+                        console.log(true);
+                        $(idName)[0].src = "file://" + directory + "images/select/" + players[index] + ".png";
+                    } else {
+                        console.log(false);
+                        $(idName)[0].src = "file://" + directory + "images/default/" + players[index] + ".png";
+                    }
+                }, 300)
+
+                var x = document.createElement("button");
+                var o = document.createElement("button");
+                x.innerHTML = "X";
+                x.id = "xButton"
+                body.appendChild(x);
+                o.innerHTML = "O";
+                o.id = "oButton"
+                body.appendChild(o);
+
+                $("#xButton").click(function() {
+                    ding.play();
+                    console.log("X");
+                    clearInterval(flashing);
+                    texts[index].innerHTML = "X";
+                    deciding = false;
+                    xPositions.push(index + 1);
+                    console.log(xPositions);
+                    $("#xButton").remove();
+                    $("#oButton").remove();
                     $(idName)[0].src = "file://" + directory + "images/select/" + players[index] + ".png";
-                } else {
-                    console.log(false);
-                    $(idName)[0].src = "file://" + directory + "images/default/" + players[index] + ".png";
-                }
-            }, 300)
+                    if ( isWinner(xPositions) ) {
+                        win.play();
+                        deciding = true; //to prevent another click
+                        winFlash(xPositions);
+                    }
+                });
 
-            var x = document.createElement("button");
-            var o = document.createElement("button");
-            x.innerHTML = "X";
-            x.id = "xButton"
-            body.appendChild(x);
-            o.innerHTML = "O";
-            o.id = "oButton"
-            body.appendChild(o);
-
-            $("#xButton").click(function() {
-                ding.play();
-                console.log("X");
-                clearInterval(flashing);
-                texts[index].innerHTML = "X";
-                deciding = false;
-                xPositions.push(index + 1);
-                console.log(xPositions);
-                $("#xButton").remove();
-                $("#oButton").remove();
-                $(idName)[0].src = "file://" + directory + "images/select/" + players[index] + ".png";
-                if ( isWinner(xPositions) ) {
-                    win.play();
-                    deciding = true; //to prevent another click
-                    winFlash(xPositions);
-                }
-            });
-
-            $("#oButton").click(function() {
-                ding.play();
-                console.log("O");
-                clearInterval(flashing);
-                texts[index].innerHTML = "O";
-                deciding = false;
-                oPositions.push(index + 1);
-                console.log(oPositions);
-                $("#xButton").remove();
-                $("#oButton").remove();
-                $(idName)[0].src = "file://" + directory + "images/select/" + players[index] + ".png";
-                if ( isWinner(oPositions) ) {
-                    win.play();
-                    deciding = true; //to prevent another click
-                    winFlash(oPositions);
-                }
-            });
+                $("#oButton").click(function() {
+                    ding.play();
+                    console.log("O");
+                    clearInterval(flashing);
+                    texts[index].innerHTML = "O";
+                    deciding = false;
+                    oPositions.push(index + 1);
+                    console.log(oPositions);
+                    $("#xButton").remove();
+                    $("#oButton").remove();
+                    $(idName)[0].src = "file://" + directory + "images/select/" + players[index] + ".png";
+                    if ( isWinner(oPositions) ) {
+                        win.play();
+                        deciding = true; //to prevent another click
+                        winFlash(oPositions);
+                    }
+                });
+            }
         }
     });
 });
